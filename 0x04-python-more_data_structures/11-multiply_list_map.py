4d3d48108caa84eba56d4d4e660c0afa778703df
lawrence307 <lawrence_muthini@yahoo.com>
2023-12-06 12:34:52 -0800

    update

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is not None:
-        students_number = len(a_dictionary.values())
-        if students_number > 0:
-            score = max(a_dictionary.values())
-            for key in a_dictionary.keys():
-                if a_dictionary[key] == score:
-                    return key
-    return None
+def mutiply_list_map(my_list=[], number=0):
+    return list(map(lambda item: item * number, my_list))
